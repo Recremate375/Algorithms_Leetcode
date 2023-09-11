f306f7d0e05163f8045669755ba0629337756872
Alexey <70941526+Recremate375@users.noreply.github.com>
2023-09-12 00:21:37 +0300

    TwoSumAlgorithm

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -2,12 +2,12 @@
 using Algorithms;
 
 var roman = new RomanToIntegerClass();
-
-Console.WriteLine(roman.RomanToInt("MCMXCIV"));
-
 var triangle = new PascalTriangle();
+var twoSum = new TwoSumClass();
 
 var list = triangle.Generate(6);
+
+Console.WriteLine(roman.RomanToInt("MCMXCIV"));
 
 foreach (var item in list)
 {
@@ -16,4 +16,11 @@ foreach (var item in list)
 		Console.WriteLine(item2.ToString());
 	}
 	Console.WriteLine("/");
+}
+
+var nums = twoSum.TwoSum(new int[] { 2, 7, 11, 15 }, 13);
+
+foreach (var item in nums)
+{
+	Console.WriteLine(item.ToString());
 }
---
+++ Algorithms/TwoSumClass.cs
@@ -0,0 +1,21 @@
+﻿namespace Algorithms
+{
+	public class TwoSumClass
+	{
+		public int[] TwoSum(int[] nums, int target)
+		{
+			for (int i = 0; i < nums.Length - 1; i++)
+			{
+				for (int j = i + 1; j < nums.Length; j++)
+				{
+					if (nums[i] + nums[j] == target)
+					{
+						return new int[] { i, j };
+					}
+				}
+			}
+
+			return Array.Empty<int>();
+		}
+	}
+}
